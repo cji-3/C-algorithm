2caf22e547f734bf4f60f3d979cbe52b326a9ecd
cji-3 <316007@st.csvs.chc.edu.tw>
2026-06-08 21:44:14 +0800

    刪除一元二次方程式解算功能的實現

--- 數學Math/.c
+++ 數學Math/.c
@@ -10,7 +10,7 @@ typedef struct{
 
 /**
  * \brief 一元二次方程式(QE)
- * \param a x^2項
+ * \param a x^2solveQuantity項
  * \param b x^1項
  * \param c 常數項
  * \return 專用的QE_return結構
@@ -19,7 +19,9 @@ QE_return* solveQE(double a, double b, double c) {
 	QE_return* out=(QE_return*)malloc(sizeof(QE_return));
 
 	if (a == 0) {
-		out->solveQuantity=0;
+		out->x1=-(c/b);
+		out->x2=out->x1;
+		out->solveQuantity=1;
 		return out;
 	}
 
